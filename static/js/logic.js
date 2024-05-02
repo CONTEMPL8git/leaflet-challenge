16eac0ddeca4650e123a819d8e4eb9179ae8c12b
CONTEMPL8git <christopherloch@hotmail.com>
2024-05-01 22:18:30 -0500

    more work done,  map appears, has markers, markers made red, etc., very much improved compared to previous

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,18 +1,34 @@
 // Creating the map object
 let myMap = L.map("map", {
-    center: [82.6416, 42.0113],
-    zoom: 7
-  });
-  
-  // Adding the tile layer
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  center: [36.6416, -97.0113],
+  zoom: 3
+});
+
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+// Load the GeoJSON data.
+let geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+
+// Get the data with d3.
+d3.json(geoData).then(function (data) {
+
+  console.log(data);
+
+  L.geoJson(data, {
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+    style: function (feature) {
+      return {
+        fillColor: "red",
+        radius: feature.properties.mag*5
+      }
+
+    }
   }).addTo(myMap);
-  
-  // Load the GeoJSON data.
-  let geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson";
-  
-  // Get the data with d3.
-  d3.json(geoData).then(function(data) {
-  
-    console.log(data);
+
+
+});
